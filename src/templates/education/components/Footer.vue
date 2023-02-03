7de64de45c44af1847c904d2f598cdf505222b9e
邱旭 <uxuip@outlook.com>
2023-02-03 18:00:25 +0800

    feat: footer component

--- src/templates/education/components/Footer.vue
+++ src/templates/education/components/Footer.vue
@@ -1,29 +1,14 @@
 <template>
-  <footer class="bg-black flex justify-center items-center">
-    <div class="text-white">
-      <div class="py-2 text-xl font-semibold">
-        {{ brandInfo.brandName }}
-      </div>
-      <div class="text-sm flex items-center">
-        <div>
-          <vc-img
-            width="120"
-            height="120"
-            :src="brandInfo.brandLogo"
-            class="border-white border-1 border-solid mr-2"
-          ></vc-img>
+  <footer class="h-20vh bg-black grid place-content-center text-white">
+    <div class="flex items-center">
+      <img src="https://picsum.photos/150" class="w-180px h-180px">
+      <div class="ml-4 flex flex-col gap-2">
+        <div class="text-3xl">
+          上海奥意特信息科技有限公司
         </div>
-        <div>
-          <p>
-            联系电话：{{ brandInfo.telepHone }}
-          </p>
-          <p>
-            联系地址：{{ brandInfo.address }}
-          </p>
-          <div class="overFlow-3">
-            文化：{{ brandInfo.introduce }}
-          </div>
-        </div>
+        <div>021-58978973</div>
+        <div>上海浦东新区浦三路3058号 2楼255室（长青企业广场）</div>
+        <div>专业从事软件定制研发以及IT服务</div>
       </div>
     </div>
   </footer>
@@ -31,6 +16,8 @@
 
 <script>
 export default {
-
+  props: {
+    info: Object,
+  },
 }
 </script>
