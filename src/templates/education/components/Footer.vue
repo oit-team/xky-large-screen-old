accc6c68548ae07906ea9ce10a7aa367a50f45e8
邱旭 <uxuip@outlook.com>
2023-02-03 20:31:03 +0800

    fix: 优化图片显示

--- src/templates/education/components/Footer.vue
+++ src/templates/education/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="h-20vh bg-black grid place-content-center text-white">
     <div class="flex items-center max-w-4/5 mx-auto">
-      <img :src="brandInfo.brandLogo" class="w-180px h-180px overflow-hidden">
+      <v-img :src="brandInfo.brandLogo" class="w-180px h-180px flex-grow-0" />
       <div class="ml-4 flex flex-col gap-2">
         <div class="text-3xl">
           {{ brandInfo.brandName }}
