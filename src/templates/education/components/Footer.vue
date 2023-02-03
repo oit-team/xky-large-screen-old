62ddf5bbd76aed5a28bf9aa03950469dc65e9d64
邱旭 <uxuip@outlook.com>
2023-02-03 19:09:10 +0800

    feat: 从接口获取品牌数据

--- src/templates/education/components/Footer.vue
+++ src/templates/education/components/Footer.vue
@@ -1,23 +1,38 @@
 <template>
   <footer class="h-20vh bg-black grid place-content-center text-white">
-    <div class="flex items-center">
-      <img src="https://picsum.photos/150" class="w-180px h-180px">
+    <div class="flex items-center max-w-4/5 mx-auto">
+      <img :src="brandInfo.brandLogo" class="w-180px h-180px">
       <div class="ml-4 flex flex-col gap-2">
         <div class="text-3xl">
-          上海奥意特信息科技有限公司
+          {{ brandInfo.brandName }}
         </div>
-        <div>021-58978973</div>
-        <div>上海浦东新区浦三路3058号 2楼255室（长青企业广场）</div>
-        <div>专业从事软件定制研发以及IT服务</div>
+        <div>{{ brandInfo.telepHone }}</div>
+        <div>{{ brandInfo.address }}</div>
+        <div>{{ brandInfo.introduce }}</div>
       </div>
     </div>
   </footer>
 </template>
 
 <script>
+import { getBrandNameCard } from '@/api/product'
+
 export default {
-  props: {
-    info: Object,
+  data: () => ({
+    brandInfo: Object,
+  }),
+
+  created() {
+    this.getBrandNameCard()
+  },
+
+  methods: {
+    async getBrandNameCard() {
+      const res = await getBrandNameCard({
+        brandId: sessionStorage.getItem('brandId'),
+      })
+      this.brandInfo = res.body
+    },
   },
 }
 </script>
